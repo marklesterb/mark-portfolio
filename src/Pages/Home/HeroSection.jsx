c2422594d2db53b21d2dd78d6b380710b6c79693
marklesterb <marklesterb22@gmail.com>
2024-02-28 17:15:55 +0800

    Added resume link in hero section

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -12,7 +12,7 @@ export default function HeroSection() {
             </h1>
               <p className='hero--section-description'>I bring a passion for creating dynamic and user-friendly websites through my proficiency in modern technologies and dedication to staying abreast of industry trends.</p>
           </div>
-          <a href='path/to/your/resume.pdf' target='_blank' className='btn btn-primary'>Resume</a>
+          <a href='https://drive.google.com/file/d/1atN9fZjvEbRPQfbP5DhqZtV63yk-Besa/view?usp=sharing' target='_blank' rel="noopener noreferrer" className='btn btn-primary'>Resume</a>
         </div>
         <div className='hero--section--img'>
           <img src='./img/profile.png' alt='Hero Section'/>
